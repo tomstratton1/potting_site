86c17af3478fab5055f96408f9a7d7ad8b02e035
tomlaptop <tstratton3912@gmail.com>
2020-11-02 10:45:26 +0000

    incorporating vue and work on main page

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+var data = {
+    maximum: 99,
+    products : [
+    {
+        "id": "1",
+        "name": "Geometric 'fifty shades' pot",
+        "description": "Geometric shapes that look great in any setting.",
+        "price": "6.99",
+        "image": "images/clean/geo_M_blue.png"
+        },
+     {
+       "id": "2",
+       "name": "Drip style pot",
+       "description": "A modern contemporary design.",
+       "price": "5.99",
+       "image": "images/drip_M.jpeg"
+     },
+     {
+        "id": "3",
+        "name": "Geometric rainbow pot",
+        "description": "A large geometric design pot.",
+        "price": "14.99",
+        "image": "images/geo_L_multi.jpeg"
+      }
+    ]
+   }
+   
+   var app = new Vue({
+    el: '#app',
+    data: data
+   });
+
+   var apptest = new Vue({
+    el: '#apptest',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
+   
